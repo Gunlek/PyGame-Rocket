c26dcf4c9bb7c2ae676cb0335f621f9dcc676f24
Fabien Aubret <fabien.aubret@gmail.com>
2017-12-13 12:31:02 +0100

    Implemented rotation

--- Simple_animation.py
+++ Simple_animation.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+import math
 
 pygame.init()
 
@@ -24,25 +25,27 @@ gravity = -9.81*50
 acc = gravity
 speed = 0
 enableFloor = True          # is there a floor on the down side of the frame
+enablePhysic = False         # Enable or disable gravity effect
 
-rocket_width = 25
-rocket_height = 50
-rocket_coords = [win_width//2-rocket_width/2, win_height-rocket_height]
+rocket_width = 50
+rocket_height = 25
+rocket_pos = [[win_width//2, win_height], math.pi/2]
 
-def renderPhysics(actualCoords, deltaT):
-    newPhysicCoords = actualCoords
+
+def renderPhysics(actualRocketPos, deltaT):
+    newPhysicCoords = actualRocketPos[0]
     global speed
     global acc
-    if actualCoords[1] < win_height-rocket_height or speed!=0:
+    if actualRocketPos[0][1] < win_height-rocket_height or speed!=0:
         acc = gravity
         dv = acc*deltaT
         speed += dv
         dy = speed*deltaT
         newPhysicCoords[1] = newPhysicCoords[1]-speed*deltaT
-    if actualCoords[1] >= win_height-rocket_height and enableFloor:
+    if actualRocketPos[0][1] >= win_height and enableFloor:
         acc=0
         speed=0
-        newPhysicCoords[1] = win_height-rocket_height
+        newPhysicCoords[1] = win_height
     return newPhysicCoords
 
 def setRocketSpeed(newSpeed):
@@ -53,18 +56,31 @@ def setRocketAcceleration(newAcceleration):
     global acc
     acc = newAcceleration
 
-def moveRocket(new_coords):
-    global rocket_coords
-    rocket_coords = new_coords
+def moveRocket(newRocketPos):
+    global rocket_pos
+    rocket_pos = newRocketPos
 
 def drawRocket():
-    rocket = pygame.draw.rect(win, GREY, (rocket_coords[0], rocket_coords[1], rocket_width, rocket_height))
+    point_1 = [int(rocket_pos[0][0]+math.sin(rocket_pos[1])*rocket_height/2), int(rocket_pos[0][1]+math.cos(rocket_pos[1])*rocket_height/2)]
+    point_2 = [int(point_1[0]+math.cos(rocket_pos[1])*rocket_width), int(point_1[1]-math.sin(rocket_pos[1])*rocket_width)]
+    point_3 = [int(point_2[0]-math.sin(rocket_pos[1])*rocket_height), int(point_2[1]-math.cos(rocket_pos[1])*rocket_height)]
+    point_4 = [int(point_3[0]-math.cos(rocket_pos[1])*rocket_width), int(point_3[1]+math.sin(rocket_pos[1])*rocket_width)]
+    
+    #rocket = pygame.draw.rect(win, GREY, (rocket_coords[0], rocket_coords[1], rocket_width, rocket_height))
+    rocket = pygame.draw.polygon(win, GREY, [point_1, point_2, point_3, point_4])
+    #rocket = pygame.draw.line(win, GREY, point_1, point_2)
+    #pygame.draw.line(win, GREY, point_2, point_3)
+    #pygame.draw.line(win, GREY, point_3, point_4)
+    #pygame.draw.line(win, GREY, point_4, point_1)
     return rocket
 
 def updateDisplay(deltaT):
     win.fill(WHITE)
-    newCoords = renderPhysics(rocket_coords, deltaT)
-    moveRocket(newCoords)
+    if enablePhysic:
+        newCoords = renderPhysics(rocket_pos, deltaT)
+    else:
+        newCoords = rocket_pos[0]
+    moveRocket([newCoords, rocket_pos[1]+math.pi/60])
     drawRocket()
     pygame.display.update()
 
@@ -72,19 +88,20 @@ def updateDisplay(deltaT):
 ## ------------- MAIN LOOP --------------- ##
 #############################################
 
+moveRocket([[rocket_pos[0][0], rocket_pos[0][1]-300], math.pi/2])
 rocket = drawRocket()
 cycle_timer = 0
 frames = 0
 refresh_timer = 0
 loop_interval = 0.001
 #setRocketSpeed(350)               #Allow user to test speed control and effects
-moveRocket([rocket_coords[0], rocket_coords[0]-90])
 
 while alive:
     if refresh_timer >= refresh_rate:
         updateDisplay(refresh_timer)
         refresh_timer = 0
         frames+=1
+        #break   #TEMPORAIRE, fige la boucle de rafraichissement
         if cycle_timer >= 1:
             cycle_timer = 0
             frames = 0
